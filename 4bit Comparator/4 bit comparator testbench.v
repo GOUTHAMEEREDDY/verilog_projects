ecb78d2c094017f0ddde8eb5525de8bf6f022310
Gouthamee Reddy <gouthameereddydumpala@gmail.com>
2025-05-14 17:11:56 +0530

    Create 4 bit comparator testbench.v

---
+++ 4bit Comparator/4 bit comparator testbench.v
@@ -0,0 +1,19 @@
+module four_bit_comparator_tb;
+ 
+reg [3:0]A;
+reg [3:0]B;
+wire A_eq_B,A_gt_B,A_lt_B;
+
+fourbitcomparator uut(.A(A),.B(B),.A_eq_B(A_eq_B),.A_gt_B(A_gt_B),.A_lt_B(A_lt_B));
+initial $monitor ("Time=%0t || A=%b B=%b|| A_eq_b=%b A_gt_b=%b A_lt_b=%b",$time,A,B,A_eq_B,A_gt_B,A_lt_B);
+
+initial begin
+ A=0011;B=1001;
+#100;
+A=1000;B=0111;
+#100;
+A=1111;B=1111;
+#100;
+end
+endmodule
+ 
