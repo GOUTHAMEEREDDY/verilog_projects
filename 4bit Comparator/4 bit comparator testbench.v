6f23031154695c1c46775c3b66486577cbf21cd3
Gouthamee Reddy <gouthameereddydumpala@gmail.com>
2025-05-14 18:47:07 +0530

    Update 4 bit comparator testbench.v

--- 4bit Comparator/4 bit comparator testbench.v
+++ 4bit Comparator/4 bit comparator testbench.v
@@ -8,11 +8,11 @@ fourbitcomparator uut(.A(A),.B(B),.A_eq_B(A_eq_B),.A_gt_B(A_gt_B),.A_lt_B(A_lt_B
 initial $monitor ("Time=%0t || A=%b B=%b|| A_eq_b=%b A_gt_b=%b A_lt_b=%b",$time,A,B,A_eq_B,A_gt_B,A_lt_B);
 
 initial begin
- A=0011;B=1001;
+ A=4'b0011;B=4'b1001;
 #100;
-A=1000;B=0111;
+A=4'b1000;B=4'b0111;
 #100;
-A=1111;B=1111;
+A=4'b1111;B=4'b1111;
 #100;
 end
 endmodule
