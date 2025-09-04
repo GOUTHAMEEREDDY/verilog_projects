8480a7d15263904ce1c85f41cfdebbbbd533f529
Gouthamee Reddy <gouthameereddydumpala@gmail.com>
2025-09-04 10:09:12 +0530

    Create CLA testbench.v

--- Carry Look Ahead Adder/CLA testbench.v
+++ Carry Look Ahead Adder/CLA testbench.v
@@ -9,12 +9,24 @@ wire Cout;
 CLA uut(.A(A),.B(B),.Cin(Cin),.Sum(Sum),.Cout(Cout));
 
 initial begin
-//Test Case 1
+//Test Case  1;
+  A = 4'b0000; B = 4'b0000; Cin = 1'b0;
+  #100;
+
+//Test Case 2
 A=4'b0001;B=4'b1001;Cin=1'b0;
 #100;
 
-//Test Case 2;
+//Test Case 3;
 A=4'b1011;B=4'b1101;Cin=1'b1;
+#100;
+
+//Testbench 4;
+A = 4'b1111; B = 4'b1111; Cin = 1'b0;
+#100;
+
+//Testbench 5: Overflow check
+A = 4'b1110; B = 4'b0011; Cin = 1'b0;
 #100;
 $finish;
 end
